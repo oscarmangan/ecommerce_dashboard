1c650d3c4cd769e2f4b3e86bfd3d55343399ae7f
Oscar Mangan <oscarmangan.ire@gmail.com>
2022-06-30 00:29:45 +0100

    initial commit, navbar and sidebar complete

---
+++ src/pages/Charts/ColourMapping.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColourMapping = () => {
+  return <div>ColourMapping</div>;
+};
+
+export default ColourMapping;
---
+++ src/pages/ColourPicker.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColourPicker = () => {
+  return <div>ColourPicker</div>;
+};
+
+export default ColourPicker;
